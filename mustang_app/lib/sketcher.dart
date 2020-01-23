745769b1811ae5fe0387048ed2235f7ff0a49bea
Arnav Kulkarni <arnavyk@gmail.com>
2020-01-22 23:20:25 -0800

    color change works but only one color at a time

--- mustang_app/lib/sketcher.dart
+++ mustang_app/lib/sketcher.dart
@@ -1,8 +1,8 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 class SketchPage extends StatefulWidget {
-
   static const String route = './Sketcher';
 
   @override
@@ -11,6 +11,63 @@ class SketchPage extends StatefulWidget {
 
 class _SketchPageState extends State<SketchPage> {
   List<Offset> points = <Offset>[];
+  Sketcher _sketcher;
+  Color pickerColor = Color(0xff443a49);
+  Color currentColor = Color(0xff443a49);
+
+  _SketchPageState() {
+    _sketcher = new Sketcher(points);
+    // _sketcher.setColor(Colors.black);
+  }
+// ValueChanged<Color> callback
+  void changeColor(Color color) {
+    setState(() => pickerColor = color);
+  }
+
+// raise the [showDialog] widget
+  void showColorPicker() {
+    showDialog(
+      context: context,
+      child: AlertDialog(
+        title: const Text('Pick a color!'),
+        content: SingleChildScrollView(
+          child: ColorPicker(
+            pickerColor: pickerColor,
+            onColorChanged: changeColor,
+            enableLabel: true,
+            pickerAreaHeightPercent: 0.8,
+          ),
+          // Use Material color picker:
+          //
+          // child: MaterialPicker(
+          //   pickerColor: pickerColor,
+          //   onColorChanged: changeColor,
+          //   enableLabel: true, // only on portrait mode
+          // ),
+          //
+          // Use Block color picker:
+          //
+          // child: BlockPicker(
+          //   pickerColor: currentColor,
+          //   onColorChanged: changeColor,
+          // ),
+        ),
+        actions: <Widget>[
+          FlatButton(
+            child: const Text('Got it'),
+            onPressed: () {
+              setState(() {
+                currentColor = pickerColor;
+                _sketcher.setColor(currentColor);
+                print("log");
+              });
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +76,7 @@ class _SketchPageState extends State<SketchPage> {
       alignment: Alignment.topLeft,
       color: Colors.blueGrey[50],
       child: CustomPaint(
-        painter: Sketcher(points),
+        painter: _sketcher,
       ),
     );
 
@@ -35,6 +92,8 @@ class _SketchPageState extends State<SketchPage> {
             point = point.translate(0.0, -(AppBar().preferredSize.height));
 
             points = List.from(points)..add(point);
+            _sketcher = new Sketcher(points);
+            _sketcher.setColor(currentColor);
           });
         },
         onPanEnd: (DragEndDetails details) {
@@ -42,13 +101,26 @@ class _SketchPageState extends State<SketchPage> {
         },
         child: sketchArea,
       ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'clear Screen',
-        backgroundColor: Colors.red,
-        child: Icon(Icons.refresh),
-        onPressed: () {
-          setState(() => points.clear());
-        },
+      floatingActionButton: SpeedDial(
+        animatedIcon: AnimatedIcons.menu_close,
+        backgroundColor: Colors.blue,
+        children: [
+          SpeedDialChild(
+            label: 'Clear Screen',
+            backgroundColor: Colors.red,
+            child: Icon(Icons.refresh),
+            onTap: () {
+              setState(() => points.clear());
+            },
+          ),
+          SpeedDialChild(
+              label: 'Palette',
+              backgroundColor: Colors.red,
+              child: Icon(Icons.format_paint),
+              onTap: () {
+                showColorPicker();
+              })
+        ],
       ),
     );
   }
@@ -56,7 +128,7 @@ class _SketchPageState extends State<SketchPage> {
 
 class Sketcher extends CustomPainter {
   final List<Offset> points;
-
+  Color _color = Colors.black;
   Sketcher(this.points);
 
   @override
@@ -64,9 +136,13 @@ class Sketcher extends CustomPainter {
     return oldDelegate.points != points;
   }
 
+  void setColor(Color color) {
+    _color = color;
+  }
+
   void paint(Canvas canvas, Size size) {
     Paint paint = Paint()
-      ..color = Colors.black
+      ..color = _color
       ..strokeCap = StrokeCap.round
       ..strokeWidth = 4.0;
 
